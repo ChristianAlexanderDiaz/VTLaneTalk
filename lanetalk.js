f08948ca1be42170087e2272cc1f390250ea2acb
ClassyCynical <98622372+ClassyCynical@users.noreply.github.com>
2023-12-13 21:26:46 -0500

    adding alpha 0.1 to the github

---
+++ lanetalk.js
@@ -0,0 +1,75 @@
+//an import statement into the script
+const puppeteer = require('puppeteer');
+const readline = require('readline');
+
+
+//an asynchronous function is something that returns a promise that can be awaited
+(async () => {
+    //launch up the browser
+    const browser = await puppeteer.launch();
+
+    //opening a new tab in the browser
+    const page = await browser.newPage();
+
+    //save the url as a new variable
+    url = 'https://beta.lanetalk.com/bowlingcenters/367895d6-d3f2-4d89-b4a6-9f11ea7af700/completed'
+
+    //navigate to the URL and wait until the page loads
+    //await means wait for the operation to complete
+    //'networkidle0' sits there until there are no more than 0 network connections for 500ms
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    
+    //the element in the HTMl to pull from that I want
+    element = '.player.ng-star-inserted';
+    //wait till the element we want is loaded
+    await page.waitForSelector(element);
+
+    // the scores will execute the function and returns a Promise
+    const scores = await page.evaluate(() => {
+    
+        // select all the elements within the class and convert the NodeList into an array
+        // ...for easy manipulation within JavaScript like 'map'
+        const players = Array.from(document.querySelectorAll('.player.ng-star-inserted'));
+        // iterate over each player element in the array
+        return players.map(player => {
+            // get the element of the name within the player element
+            const text = player.querySelector('.nameAndDate > .name').innerText;
+
+            // split the text because of the null character
+            const [name, team] = text.split('\n');
+
+            // these contain the order of the scores starting with '0' for the 1st game
+            const scoreElements = player.querySelectorAll('.numbersWrapper > .numberBatch');
+            // convert the NodeList into an array with the scores
+            const scores = Array.from(scoreElements).map(el => el.innerText);
+
+            // contains the total score from the website
+            // !!! THIS WILL SOON BE CHANGED TO FIRST TWO GAMES FOR BOWLING CLUB !!!
+            const totalScore = player.querySelector('.totalScore > .score').innerText;
+
+            // return all of the content gathered
+            return { name, team, scores, totalScore };
+    });
+  });
+    //the console is placing the scores in the terminal 
+    console.log(scores);
+
+    //close the browser to finish
+    await browser.close();
+
+      // Filter for a specific bowler
+  const bowlerName = 'Christian Diaz'; // Replace with the desired name
+  const filteredScores = scores.filter(player => player.name.includes(bowlerName));
+
+  // Loop through each player and ask for confirmation
+  for (let data of filteredScores) {
+    const isConfirmed = await askConfirmation(data);
+    if (isConfirmed) {
+      console.log('Confirmed:', data);
+      // Here you would add the code to push to Google Sheets
+    } else {
+      console.log('Not confirmed:', data);
+    }
+  }
+  rl.close();
+})();
