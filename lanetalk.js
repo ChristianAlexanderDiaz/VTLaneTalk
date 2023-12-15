9e3e31871798581bb7632951e1518c97437e98e3
ClassyCynical <98622372+ClassyCynical@users.noreply.github.com>
2023-12-15 16:35:54 -0500

    added menu with extras

--- lanetalk.js
+++ lanetalk.js
@@ -1,57 +1,72 @@
-//an import statement into the script
 const puppeteer = require('puppeteer');
+const readline = require('readline');
+const { spawn } = require('child_process');
+const fs = require('fs');
 
-// an asynchronous function is something that returns a promise that can be awaited
-(async () => {
-    //launch up the browser
-    const browser = await puppeteer.launch();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-    //opening a new tab in the browser
-    const page = await browser.newPage();
+const namesToFind = ['Kiki', 'Kristen', 'Eric', 'Susan', 'Kellie'];
 
-    //save the url as a new variable
-    url = 'https://beta.lanetalk.com/bowlingcenters/367895d6-d3f2-4d89-b4a6-9f11ea7af700/completed'
-
-    //navigate to the URL and wait until the page loads
-    //await means wait for the operation to complete
-    //'networkidle0' sits there until there are no more than 0 network connections for 500ms
-    await page.goto(url, { waitUntil: 'networkidle0' });
-    
-    //the element in the HTMl to pull from that I want
-    element = '.player.ng-star-inserted';
-    //wait till the element we want is loaded
-    await page.waitForSelector(element);
-
-    // the scores will execute the function and returns a Promise
-    const scores = await page.evaluate(() => {
-    
-        // select all the elements within the class and convert the NodeList into an array
-        // ...for easy manipulation within JavaScript like 'map'
-        const players = Array.from(document.querySelectorAll('.player.ng-star-inserted'));
-        // iterate over each player element in the array
-        return players.map(player => {
-            // get the element of the name within the player element
-            const text = player.querySelector('.nameAndDate > .name').innerText;
-
-            // split the text because of the null character
-            const [name, team] = text.split('\n');
-
-            // these contain the order of the scores starting with '0' for the 1st game
-            const scoreElements = player.querySelectorAll('.numbersWrapper > .numberBatch');
-            // convert the NodeList into an array with the scores
-            const scores = Array.from(scoreElements).map(el => el.innerText);
-
-            // contains the total score from the website
-            // !!! THIS WILL SOON BE CHANGED TO FIRST TWO GAMES FOR BOWLING CLUB !!!
-            const totalScore = player.querySelector('.totalScore > .score').innerText;
-
-            // return all of the content gathered
-            return { name, team, scores, totalScore };
+const askForConfirmation = async (namesAndScores) => {
+  return new Promise(resolve => {
+    rl.question(`Found the following Bowling Club members:\n${namesAndScores.join('\n')}\nWould you like to push this to Google Sheets? (Y/N) `, (answer) => {
+      resolve(answer.toLowerCase());
     });
   });
-    //the console is placing the scores in the terminal 
-    console.log(scores);
+};
 
-    //close the browser to finish
+const showAllData = (scores) => {
+  console.log('All gathered data:');
+  console.log(scores.map(player => `${player.name} (${player.team}): ${player.scores}`).join('\n'));
+};
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://beta.lanetalk.com/bowlingcenters/367895d6-d3f2-4d89-b4a6-9f11ea7af700/completed', { waitUntil: 'networkidle0' });
+
+  await page.waitForSelector('.player.ng-star-inserted');
+
+  const scores = await page.evaluate(() => {
+    const players = Array.from(document.querySelectorAll('.player.ng-star-inserted'));
+    return players.map(player => {
+      const nameText = player.querySelector('.nameAndDate > .name').innerText;
+      const [name, team] = nameText.split('\n');
+      const scoreElements = player.querySelectorAll('.numbersWrapper > .numberBatch');
+      const scores = Array.from(scoreElements).map(el => el.innerText).join(', ');
+      return { name, team, scores };
+    });
+  });
+
+  const matchedScores = scores.filter(player =>
+    namesToFind.some(nameToFind =>
+      player.name.toLowerCase().includes(nameToFind.toLowerCase())
+    )
+  );
+
+  const namesAndScores = matchedScores.map(player => `${player.name} (${player.team}): ${player.scores}`);
+  
+  const confirmation = await askForConfirmation(namesAndScores);
+  
+  if (confirmation === 'y') {
+    console.log('Confirmed. Pushing to Google Sheets...');
+    // Code to push to Google Sheets goes here
+  } else if (confirmation === 'n') {
+    rl.question('Show all gathered data? (Y/N) ', async (showAll) => {
+      if (showAll.toLowerCase() === 'y') {
+        showAllData(scores);
+      } else {
+        console.log('Exiting program.');
+      }
+      rl.close();
+      await browser.close();
+    });
+  } else {
+    console.log('Invalid input. Exiting program.');
+    rl.close();
     await browser.close();
+  }
 })();
